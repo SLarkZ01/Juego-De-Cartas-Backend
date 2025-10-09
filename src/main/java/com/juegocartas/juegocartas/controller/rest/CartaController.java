33ddeeac2b9a9079ee3d71637bb24a892f9cbda5
SLarkZ01 <tmontoyamagon11@gmail.com>
2025-10-09 05:53:48 -0500

    Documentacion ome

--- src/main/java/com/juegocartas/juegocartas/controller/rest/CartaController.java
+++ src/main/java/com/juegocartas/juegocartas/controller/rest/CartaController.java
@@ -9,11 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.oas.annotations.Operation;
 
 import com.juegocartas.juegocartas.model.Carta;
 import com.juegocartas.juegocartas.service.CartaService;
 import com.juegocartas.juegocartas.service.DragonBallApiService;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/api/cartas")
