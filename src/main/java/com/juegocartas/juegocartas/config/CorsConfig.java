80402ead1c0ca78c9925172c858ca07f22622644
SLarkZ01 <tmontoyamagon11@gmail.com>
2025-10-09 04:27:02 -0500

    Se aplico cors para conectar con NextJS

---
+++ src/main/java/com/juegocartas/juegocartas/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.juegocartas.juegocartas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
